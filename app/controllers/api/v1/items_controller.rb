e352efdc41e6e01a99b5cbc78a85df55c8e67435
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-01-23 14:00:58 -0700

    Added CSV Data file
    added all needed CSV's files.

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -8,6 +8,13 @@ class Api::V1::ItemsController < ApplicationController
   end
 
 	def create
-
+    render json: Item.create(item_params)
 	end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description)
+  end
+  
 end
