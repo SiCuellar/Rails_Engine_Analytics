70166e13ebcbf93904f076c7b9c0646009febd23
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-01-23 17:00:39 -0700

    Added more model relationship testing

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it { should validate_presence_of(:status)}
+  end
+
+  describe 'Relationships' do
+    it { should have_many(:invoice_items)}
+    it { should have_many(:transactions)}
+    it { should have_many(:items).through(:invoice_items)}
+
+    it { should belong_to(:customer)}
+    it { should belong_to(:merchant)}
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it { should validate_presence_of(:name)}
+  end
+
+  describe 'Relationships' do
+    it { should have_many(:items)}
+    it { should have_many(:invoices)}
+    it { should have_many(:invoice_items).through(:invoices)}
+  end
 end
