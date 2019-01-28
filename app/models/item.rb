8d71a6d3a4010285ab43abc52b85498548b19f4c
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-01-27 17:15:25 -0700

    Added top item by revenue

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,11 @@ class Item < ApplicationRecord
 
   belongs_to :merchant
   has_many :invoice_items
+
+  def self.top_item_revenue(number)
+    joins(:invoice_items)
+    .group(:id)
+    .order("sum(invoice_items.quantity * invoice_items.unit_price) desc")
+    .limit(number)
+  end
 end
